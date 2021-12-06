6e815e1497fac07d10719020e035dd8ad5eaba9b
Shawn Philipps <89462917+ThePhreshmaker@users.noreply.github.com>
2021-12-06 09:30:08 -0500

    Update pokemon_script.js

--- pokemon_script.js
+++ pokemon_script.js
@@ -1,4 +1,4 @@
-/* Author: Shawn Philipps*/
+/* Author: Shawn Philipps */
 /* Javascript script that will retrieve information from Pokemon API to be used in CSS and HTML document */
 /* This script will only retrieve the first 151 Pokemon (original pokemon) */
 /* Get "pokedex" element from HTML page */
@@ -9,9 +9,9 @@ const fetchPokemon = () => {
 /* array to hold Objects from API */
   const promises = []
   /* Loop that will loop through first 151 pokemon in PokeAPI. 'i' is used as a parameter in URL link to API endpoint and corresponds to pokemon id number */
-  for (let i = 1; i <= 151; i++){
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    /*push json obects into promises array*/
+    /* push json obects into promises array */
     promises.push(fetch(url).then((res) => res.json()))
   }
   /* Promise that will take the array of objects call "promises" and queue the contents and load each result in parallel */
@@ -22,11 +22,11 @@ const fetchPokemon = () => {
       id: data.id,
       image: data.sprites.front_default,
       /* converting weight and height fields to kilograms and meters respectively */
-      weight: (data.weight * .1).toFixed(2),
-      height: (data.height * .1).toFixed(2),
+      weight: (data.weight * 0.1).toFixed(2),
+      height: (data.height * 0.1).toFixed(2),
       /* mapping abilties and joining with a comma for easy formatting. Same with types */
       abilities: data.abilities.map(ability => ability.ability.name).join(', '),
-      type: data.types.map( type => type.type.name).join(', ')
+      type: data.types.map(type => type.type.name).join(', ')
     }))
     displayPokemon(pokemon)
   })
